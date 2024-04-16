b2104d7a33eb23005d6c145b3949b10ec5031a8d
Filip Hadzi-Ristic <filip.hadzi.ristic@msg-global.com>
2024-04-16 18:58:00 +0200

    Hourly_Jackpot_Logger: Moved Strings to config.properties file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,35 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static constants.PropertyConstants.MESSAGE_CURRENCY;
+import static constants.PropertyConstants.MESSAGE_GREETING;
+import static constants.PropertyConstants.JACKPOT_NAME;
+
 
 public class Main
 {
-		public static void main(String[] args) {
-			// Greet the user
-			System.out.println("Welcome to the Hourly Jackpot Scraper!");
+	public static void main(String[] args)
+	{
+		// Greet the user
+		System.out.println(MESSAGE_GREETING);
 
-			// Ask for currency input
-			System.out.println("Please enter the currency (e.g., USD, EUR):");
-			Scanner scanner = new Scanner(System.in);
-			String currency = scanner.nextLine().toLowerCase();
+		// Ask for currency input
+		System.out.println(MESSAGE_CURRENCY);
+		Scanner scanner = new Scanner(System.in);
+		String currency = scanner.nextLine().toLowerCase();
 
-			// Set up timer to call HourlyJackpotScraperService
-			Timer timer = new Timer();
-			timer.scheduleAtFixedRate(new TimerTask() {
-				@Override
-				public void run() {
-					String jackpotAmount = HourlyJackpotScraperService.scrapeHourlyJackpot(currency);
-					System.out.println("Hourly Jackpot: " + jackpotAmount + " " + currency.toUpperCase());
-				}
-			}, 0, 30);
+		// Set up timer to call HourlyJackpotScraperService
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask()
+		{
+			@Override
+			public void run()
+			{
+				String jackpotAmount = HourlyJackpotScraperService.scrapeHourlyJackpot(currency);
+				System.out.println(JACKPOT_NAME + ": " + jackpotAmount + " " + currency.toUpperCase());
+			}
+		}, 0, 30);
 
-			scanner.close();
-		}
+		scanner.close();
+	}
 }
--- src/main/java/constants/PropertyConstants.java
+++ src/main/java/constants/PropertyConstants.java
@@ -7,11 +7,15 @@ public abstract class PropertyConstants
 {
 	public static final String REQUEST_URL;
 	public static final String JACKPOT_NAME;
+	public static final String MESSAGE_GREETING;
+	public static final String MESSAGE_CURRENCY;
 
 	static
 	{
 		ResourceBundle bundle = ResourceBundle.getBundle("config");
 		REQUEST_URL = bundle.getString("request.url");
 		JACKPOT_NAME = bundle.getString("jackpot.name");
+		MESSAGE_GREETING = bundle.getString("message.greeting");
+		MESSAGE_CURRENCY = bundle.getString("message.currency");
 	}
 }
